2db5d2f9277727a3c06119af386f5edf3312810b
victordanielquino <victordanielquino@gmail.com>
2022-08-30 13:25:42 -0400

    add: conection data base and document with swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { enviroments } from './common/config/enviroments';
+import * as Joi from 'joi';
+import config from './common/config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env.dev',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // se llegan atributos que no estan definidos en el dtos, los ignora y continua
+      forbidNonWhitelisted: true, // alerta de atributos que no esta definido en el esquema de los dtos
+      transformOptions: { enableImplicitConversion: true }, // convierte string a number en @Query params
+    }),
+  );
+
+  // INTERCEPTOR:
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('API USER V1')
+    .setDescription('DOCUMENTS OF API BY VICTOR QUINO')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('API MICROSERVICES USER')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/v1/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
+  app.setGlobalPrefix('api/v1');
+
+  // habilitar acceso a todos CORS:
+  app.enableCors();
+  // FILTRO PARA CAPTURAR ERRORES
+  app.useGlobalFilters(new AllExceptionFilter());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
